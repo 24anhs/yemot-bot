096e011bcfe08c857e0348f65ff7a69ad316a681
24anhs <24anhs@gmail.com>
2026-02-25 18:34:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, request
-import os
 
-app = Flask(name)
-
-@app.route('/')
-def home():
-return "Server is Up!"
+app = Flask(__name__)
 
 @app.route('/yemot', methods=['GET', 'POST'])
 def yemot_api():
-phone = request.values.get('ApiPhone', 'unknown')
-response = "id_list_message=t-Shalom, your phone number is " + phone + "&hangup"
-return response
+    # קבלת הנתונים שימות המשיח שלחו אלינו
+    caller_phone = request.values.get('ApiPhone') # מספר הטלפון של המחייג
+    user_input = request.values.get('ID_Input')   # משתנה שאנחנו נגדיר בשלב הבקשה
 
-if name == "main":
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+    # אם עדיין אין לנו קלט מהלקוח, נבקש ממנו להקיש נתונים
+    if not user_input:
+        # פקודת read: מבקשת מהלקוח להקיש נתונים. 
+        # המבנה: read=קובץ_שמע=שם_משתנה_לחזרה,מספר_ספרות...
+        # t-אנא... = השמעת טקסט (Text to Speech)
+        return "read=t-אנא הקש את תעודת הזהות שלך וסולם=ID_Input,no,9,9,7,Ok,True,True"
+    
+    # אם כבר קיבלנו את הקלט מהלקוח
+    else:
+        # פקודת id_list_message: משמיעה הודעה
+        # פקודת hangup: מנתקת את השיחה
+        return f"id_list_message=t-שלום למספר טלפון {caller_phone}. תעודת הזהות שהקשת היא {user_input}. תודה ולהתראות&hangup"
+
+if __name__ == '__main__':
+    # הפעלת השרת על פורט 5000
+    app.run(port=5000)
